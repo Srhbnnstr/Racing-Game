e842e92cd12346681de8d2385943a5d684dba550
srhbnnstr <srhbnnstr@gmail.com>
2016-03-26 19:48:19 -0700

    added movement to players

--- scripts/app.js
+++ scripts/app.js
@@ -1,56 +1,19 @@
 $(document).ready(function() {
 
+  console.log('JS is loaded!');
+
   $reset = $('#reset');
-  var winner = null;
 
-  // reset to initial state of race
-  var raceAgain = function() {
-    $reset.addClass('tmp-hidden');
-    $('.player').css({left: 0});
-    winner = null;
-  };
-
-  // show confetti and reset button when winner declared
-  var executeWin = function() {
-    $confetti.css({'z-index': 1, opacity: 1});
-    $reset.removeClass('tmp-hidden');
-  };
-
-  // take keypress event and move correct player
-  var movePlayer = function(keypressEvent) {
-
-    // returns key like 'a', 'l', etc.
-    var keyCode = String.fromCharCode(keypressEvent.keyCode);
-
-    // find player and player's position
-    var $player = $('[data-key="' + keyCode + '"]');
-    var leftPosition = $player.offset().left;
-
-    // set player's new position (move forward)
-    $player.css({left: leftPosition + 10});
-
-    // if player moved past end of track
-    if ($player.offset().left >= $('#track').width() - $player.width()) {
-
-      // set winner to player and execute win
-      winner = $player;
-      executeWin();
-    }
-  };
-
-  // add event-handlers
-  var race = function() {
-    $(window).on('keypress', function(event) {
-      if (!winner) {
-        movePlayer(event);
+  $('body').keydown(function(e) {
+    var offset = 8.5;
+      if(e.keyCode == '65') {
+        var pos = $("#player1").position();
+      $("#player1").css('left', pos.left + offset);
+      } else if(e.keyCode == '80') {
+        var pos = $("#player2").position();
+        $("#player2").css('left',pos.left + offset);
       }
-    });
-    $reset.on('click', function() {
-      raceAgain();
-    });
-  };
+  });
 
-  // start the race!
-  race();
 
 });
