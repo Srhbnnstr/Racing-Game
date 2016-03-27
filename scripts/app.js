9cf692ea3334a989c68c9eef2bf71472ae1bc5d5
srhbnnstr <srhbnnstr@gmail.com>
2016-03-27 10:53:49 -0700

    added alert for win, need to fix

--- scripts/app.js
+++ scripts/app.js
@@ -3,23 +3,35 @@ $(document).ready(function() {
   console.log('JS is loaded!');
 
   $reset = $('#reset');
+  $player = $('#player1, #player2');
+  var winner = null;
 
-  //reset player position
+//resets game
   var race= function() {
-    $('#player1').css({left: 0});
-    $('#player2').css({left: 0});
+    $player.css({left: 0});
+  };
+
+  var executeWin = function() {
+    alert("Well done.");
+    race();
   };
 
  //assigns keycode to each player, moves player with correct keypress
   $('body').keydown(function(e) {
     var offset = 10;
       if(e.keyCode == '65') {
-        var pos = $("#player1").position();
+      var pos = $("#player1").position();
       $("#player1").css('left', pos.left + offset);
       } else if(e.keyCode == '80') {
-        var pos = $("#player2").position();
+      var pos = $("#player2").position();
         $("#player2").css('left',pos.left + offset);
       }
+
+      // player reaches finish line
+  if ('#player1' || '#player2'.offset().left >= $('#tracks').width() - $player.width()) {
+    var winner = $player;
+    executeWin();
+  }
 
     $reset.on('click', function() {
      race();
