ddb845f8bdbd8343bcb34b768ecebc73f8e0d147
srhbnnstr <srhbnnstr@gmail.com>
2016-03-28 09:07:34 -0700

    fixed spacing, added instructions

--- scripts/app.js
+++ scripts/app.js
@@ -2,39 +2,42 @@ $(document).ready(function() {
 
   console.log('JS is loaded!');
 
+   //Audio controls
+
   var yourAudio = document.getElementById('yourAudio'),
     ctrl = document.getElementById('audioControl');
 
-ctrl.onclick = function () {
+  ctrl.onclick = function () {
 
     // Update the Button
-    var pause = ctrl.innerHTML === 'pause!';
+  var pause = ctrl.innerHTML === 'pause!';
     ctrl.innerHTML = pause ? 'play!' : 'pause!';
 
     // Update the Audio
-    var method = pause ? 'pause' : 'play';
+  var method = pause ? 'pause' : 'play';
     yourAudio[method]();
 
     // Prevent Default Action
-    return false;
+  return false;
   };
 
+    //global variables
   var $reset = $('#reset');
   var $player = $('.player');
   var winner= 'null';
 
-
-//resets game
+    //resets game
   var race= function() {
     $player.css({left: 0});
   };
 
+   //when a winner is determined
   var executeWin = function() {
     alert("Hot damn, " + winner + "! You win!" );
     race();
   };
 
- //assigns keycode to each player, moves player with correct keypress
+   //assigns keycode to each player, moves player with correct keypress
   $('body').keydown(function(e) {
     var offset = 10;
       if(e.keyCode == '65') {
@@ -45,14 +48,16 @@ ctrl.onclick = function () {
         $("#p2").css('left',pos.left + offset);
       }
 
-        if ($("#p1").offset().left >= $('#tracks').width() - $player.width()) {
-          winner = 'player 1';
-          executeWin();
-        } else if ($("#p2").offset().left >= $('#tracks').width() - $player.width()) {
-          winner = 'player 2';
-          executeWin();
+    //determines a winner
+    if ($("#p1").offset().left >= $('#tracks').width() - $player.width()) {
+        winner = 'player 1';
+        executeWin();
+    } else if ($("#p2").offset().left >= $('#tracks').width() - $player.width()) {
+        winner = 'player 2';
+        executeWin();
         }
 
+    //reset button
     $reset.on('click', function() {
      race();
    });
